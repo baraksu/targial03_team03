3122e6ce21b57cb1480702e9aabbc5262ee0bfcb
YotamOphir <yotam4ophir@gmail.com>
2025-11-25 16:09:37 +0200

    add text

--- Encryption.java
+++ Encryption.java
@@ -1,14 +1,14 @@
 /**
  * Write a description of class Encryption here.
  *
- * @author (your name)
+ * @author Yotam, Etan, Ariel
  * @version (a version number or a date)
  */
 public class Encryption
 {
     public static void main(String[] args){
         
-        System.out.println("Welcome to the Encryption / Decryption Program");
+        System.out.println("Welcome to the Encryption / Decryption Program \nEnter 1 for Encrypt | 2 for Decrypt");
         
     }
     public static int add (int a, int b){
